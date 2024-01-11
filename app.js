bd7d7dda4f08ccc5f7b9ea54e55e6d49c764011e
Lite <lite.th745@gmail.com>
2024-01-11 15:11:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" });
+const path = require('path');
+
+app.use(bodyParser.json());
+bodyParser.urlencoded({extended: true})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); 
+
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+const parser = new xml2js.Parser();
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.post("/upload", upload.single('Xmlfile'), async (req, res) => {
+
+  let fileName = './uploads/' + req.file.filename;
+  let xmlData = fs.readFileSync(fileName, 'utf-8');
+
+  console.log(xmlData);
+  parser.parseString(xmlData, function (err, result) {
+    if(!err) {
+      fs.writeFileSync("./public/Output.json", JSON.stringify(result));
+    }
+  });
+
+  fs.rm(fileName, () => {});
+
+  res.render('download');
+});
+
+app.get('/download', (req, res) => {
+  let filePath = path.join(__dirname + '/public/Output.json');
+  res.download(filePath, () => {
+    fs.rm('./public/Output.json', () => {});
+  });
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
